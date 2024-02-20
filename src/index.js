c25147c3a2f3dfc3f6040175c4bd0e7d3774936e
dev0869 <deveshbisht36@gmail.com>
2024-02-20 17:14:58 +0530

    sdsd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
 import { authRoute } from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
-import os from "node:os";
 import cors from "cors";
 dotenv.config();
 const app = express();
@@ -15,16 +13,10 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(connectDb)
+app.use(connectDb);
 app.use(cookieParser());
 app.use("/api", authRoute);
 
-
-
-    app.listen(port, () => {
-      console.log(`Server is litening on http://localhost:${port}  /`);
-    })
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+app.listen(port, () => {
+  console.log(`Server is litening on http://localhost:${port}  /`);
+});
