98f222c7b578101dd96bd0998073d287be56ed16
dev0869 <deveshbisht36@gmail.com>
2024-03-05 10:39:54 +0530

    jwt auth implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,9 @@
-import { Route, Routes } from "react-router-dom";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import { useEffect } from "react";
-import axios from "axios";
-import { option, option2 } from "./utils";
+import { TextGenerateEffect } from "./components/auth/ui/TextGenerateEffect";
+
+const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows`;
+
 const App = () => {
-  useEffect(() => {
-    const verify = async () => {
-      try {
-        const res = await axios.post(
-          "http://localhost:8000/api/user/Verify",
-          // {},
-          // option
-        );
-        console.log(res.data);
-      } catch (error) {
-        if (error.response.data.message === "jwt expired") {
-          const res1 = await axios.post(
-            "http://localhost:8000/api/user/refreshAccessToken",
-            {},
-            option2
-          );
-          localStorage.setItem("AccessToken", res1.data.data);
-        }
-        console.log(error);
-      }
-    };
-    verify();
-  }, []);
+  return <TextGenerateEffect words={words} />;
+}
 
-  return (
-    <>
-      <Routes>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-    </>
-  );
-};
-
-export default App;
+export default App
